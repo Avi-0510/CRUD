caeccea3d85313bdcc751f7a4ebaea181b82eff4
Arpan Kumar <arpan.kumar@interbizconsulting.com>
2021-01-30 17:49:51 +0530

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,29 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { CreateEmployeeComponent } from './pages/create-employee/create-employee.component';
+import { UpdateEmployeeComponent } from './pages/update-employee/update-employee.component';
+
+import {RouterModule, Routes} from '@angular/router'
+
+const routes: Routes = [
+  {path: "create" , component: CreateEmployeeComponent},
+  {path: "update" , component: UpdateEmployeeComponent},
+  {path: "" , redirectTo: '/create' ,pathMatch: 'full' },
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    CreateEmployeeComponent,
+    UpdateEmployeeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    [RouterModule.forRoot(routes)]
   ],
   providers: [],
   bootstrap: [AppComponent]
